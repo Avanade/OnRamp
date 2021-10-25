d89599b2a99d68b983af91c2d249d59c4f2e0d29
Eric Sibly <eric.sibly@avanade.com>
2021-10-25 12:51:22 -0700

    Add Warn message to test to debug on github.

--- tests/OnRamp.Test/CodeGeneratorTest.cs
+++ tests/OnRamp.Test/CodeGeneratorTest.cs
@@ -291,6 +291,7 @@ namespace OnRamp.Test
 
             var cg = new CodeGenerator(new CodeGeneratorArgs("ValidEntity.yaml") { ExpectNoChanges = true }.AddAssembly(typeof(CodeGeneratorTest).Assembly).AddParameter("Directory", "F140"));
             var ex = Assert.Throws<CodeGenChangesFoundException>(() => cg.Generate("Data/ValidEntity.yaml"));
+            Assert.Warn(ex.Message);
             Assert.IsTrue(ex.Message.EndsWith("F140\\Person.txt' would be created as a result of the code generation."));
         }
 
